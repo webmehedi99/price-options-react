cc6d2ce8f54ad134e2e0291f877646ba7327dc79
webmehedi99 <webmehedi99@gmail.com>
2024-10-29 22:05:30 +0600

    create navbar and link file

---
+++ src/Components/Link/Link.jsx
@@ -0,0 +1,12 @@
+
+
+const Link = ({ route }) => {
+    return (
+        <div>
+            <li className="mr-10" >
+                <a href={route.path}>{route.name}</a></li>
+        </div>
+    );
+};
+
+export default Link;
